1d2b13201a2a42e5508b859152f65c10f99de695
Javid Sadigli <cavidoffical@gmail.com>
2025-09-05 11:17:35 +0400

    Added get student by id endpoint

--- controllers/student_controller.py
+++ controllers/student_controller.py
@@ -3,3 +3,6 @@ from models.student import Student
 
 def get_all_students(db: Session):
     return db.query(Student).all()
+
+def get_student_by_id(db: Session, student_id: int):
+    return db.query(Student).filter(Student.id == student_id).first()
--- routers/student_router.py
+++ routers/student_router.py
@@ -8,3 +8,10 @@ router = APIRouter()
 @router.get("/")
 def get_all_students(db: Session = Depends(get_db)):
     return student_controller.get_all_students(db)
+
+@router.get("/{student_id}")
+def get_student(student_id: int, db: Session = Depends(get_db)):
+    student = student_controller.get_student_by_id(db, student_id)
+    if not student:
+        return {"error": "Student not found"}
+    return student
